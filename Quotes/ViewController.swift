798e552ef111ff7eda1d5bda9912da7f4cd20232
Oleg <olegpsiarovsky@gmail.com>
2019-02-14 00:19:47 +0200

    Create simple project

--- Quotes/ViewController.swift
+++ Quotes/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var quoteTextLabel: UILabel!
+    @IBOutlet weak var authorImageView: UIImageView!
+    @IBOutlet weak var authorNameLabel: UILabel!
+    
+    let quotes = ["Your time is limited. So don't waste it living someone else's life.",
+                  "No matter how small you start, start somethings that matters",
+                  "Veni, vidi, vici."
+    ]
+    
+    let authorNames = ["Steve Jobs", "Brendon Burchard", "Julius Caesar"]
+    
+    let imageNames = ["stevejobs", "brendonburchard", "juliuscaesar"]
+    
+    @IBAction func inspireMeDidTap(_ sender: Any) {
+        
+        let randomNumber = Int(arc4random()) % 3
+        
+        let newQuote = quotes[randomNumber]
+        let newAuthor = authorNames[randomNumber]
+        let authorImageName = imageNames[randomNumber]
+        
+        quoteTextLabel.text = newQuote
+        authorNameLabel.text = newAuthor
+        authorImageView.image = UIImage(named: authorImageName)
+        
     }
-
-
 }
 
